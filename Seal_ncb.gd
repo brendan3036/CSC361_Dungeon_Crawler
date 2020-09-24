7ab8c7b6ad31ad971cd7d9e335fd19510810f2e9
Nate Bream <nb7249@cs.ship.edu>
2020-09-23 23:33:32 -0400

    my stuff

--- Seal_ncb.gd
+++ Seal_ncb.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
@@ -10,18 +10,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var movement = Vector2(0, 0)
+	
 	if Input.is_action_pressed('up'):
-		position.y -= 1
+		movement = Vector2(0, -1)
 		get_node("AudioStreamPlayer").play()
 		#sealNoise.play()
 	if Input.is_action_pressed('left'):
-		position.x -= 1
+		movement = Vector2(-1, 0)
 		get_node("AudioStreamPlayer").play()
 	if Input.is_action_pressed('down'):
-		position.y += 1
+		movement = Vector2(0, 1)
 		get_node("AudioStreamPlayer").play()
 	if Input.is_action_pressed('right'):
-		position.x += 1
+		movement = Vector2(1, 0)
 		get_node("AudioStreamPlayer").play()
+		
+	self.move_and_collide(movement)
 	
 	
