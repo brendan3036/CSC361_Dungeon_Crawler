eaa51321e930eb3c9b179091c1d9de7a13133c67
Brendan Karper <bk3036@ship.edu>
2020-11-04 17:48:48 -0500

    volume sliders, main menu continuity, fixed part of skeleton sprite clipping

---
+++ MusicVolumeSlider.gd
@@ -0,0 +1,13 @@
+extends HSlider
+
+
+export var audio_bus_name := "Music"
+onready var _bus = AudioServer.get_bus_index(audio_bus_name)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	value = db2linear(AudioServer.get_bus_volume_db(_bus))
+	
+func _on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(_bus, linear2db(value))
---
+++ VolumeSlider.gd
@@ -0,0 +1,15 @@
+extends HSlider
+
+
+export var audio_bus_name := "Master"
+onready var _bus = AudioServer.get_bus_index(audio_bus_name)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	value = db2linear(AudioServer.get_bus_volume_db(_bus))
+	
+func _on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(_bus, linear2db(value))
+
+
