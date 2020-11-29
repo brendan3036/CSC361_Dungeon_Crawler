056dfcf6b4dcab7fd44237c32c25d5fdf3534117
Brendan Karper <bk3036@ship.edu>
2020-11-28 22:37:22 -0500

    started 2nd level design

--- stairs.gd
+++ stairs.gd
@@ -2,17 +2,21 @@ extends Area2D
 
 
 var buttonPressFlag = 0
-
+var parentName
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	parentName = get_parent().name
 	pass
 		
 
 func _process(delta):
 	if buttonPressFlag == 1 and Input.is_action_pressed('use'):
-		# clear the tilemap, keep the player, load the new instance
-		print(get_parent().get_parent().name)
-		get_parent().get_parent().show_map("tileMap_2")
+		if parentName == "TileMap":
+			# clear the tilemap, keep the player, load the new instance
+			get_parent().get_parent().show_map("tileMap_2")
+		
+		
+		
 #		for obj in get_tree().get_root().get_children():
 #			if obj.is_in_group("gamescene"):
 #				obj.clear()
